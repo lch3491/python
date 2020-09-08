708d981f05bd7750a6921c86340ad90e8b27ce36
lch3491 <lch3491@gmail.com>
2020-09-08 22:52:29 +0900

    crawing 2 file

---
+++ crawing_3.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://comic.naver.com/webtoon/weekday.nhn"
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, 'lxml')
+print(soup.title)
+print(soup.head)
